6850bf707ee5ac13763c99c04cf893a8065c54e2
bigbums <bigbums@gmail.com>
2026-03-03 22:50:32 +0100

    Fix admin duplication: keep Tenant/Tier/TenantUser in core, refine CustomUserAdmin with Profile inline and tenant info

--- saas_app/accounts/admin.py
+++ saas_app/accounts/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import CustomUser, Profile
+from saas_app.core.models import TenantUser
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = "Profile"
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ("email", "first_name", "last_name", "is_staff", "is_active")
-    list_filter = ("is_staff", "is_active")
+    inlines = [ProfileInline]
+    list_display = ("email", "first_name", "last_name", "tenant_name", "tenant_tier", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active", "is_superuser")
     ordering = ("email",)
     search_fields = ("email", "first_name", "last_name")
 
@@ -20,6 +27,16 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = (
         (None, {
             "classes": ("wide",),
-            "fields": ("email", "password1", "password2", "is_staff", "is_active"),
+            "fields": ("email", "first_name", "last_name", "password1", "password2", "is_staff", "is_active"),
         }),
     )
+
+    def tenant_name(self, obj):
+        tenant_user = TenantUser.objects.filter(user=obj).first()
+        return tenant_user.tenant.name if tenant_user else "-"
+    tenant_name.short_description = "Tenant"
+
+    def tenant_tier(self, obj):
+        tenant_user = TenantUser.objects.filter(user=obj).first()
+        return tenant_user.tenant.tier.name if tenant_user and tenant_user.tenant.tier else "-"
+    tenant_tier.short_description = "Tier"
