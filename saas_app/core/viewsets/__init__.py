1ffff498f6dbd416905d0f5147f9c3b8ef6d6dca
bigbums <bigbums@gmail.com>
2026-03-04 19:49:06 +0100

    Fix ImportError: import viewsets directly instead of via __init__.py

--- saas_app/core/viewsets/__init__.py
+++ saas_app/core/viewsets/__init__.py
@@ -1,10 +1,3 @@
-from .base import BaseTenantViewSet
-from .viewsets import (
-    ItemViewSet, SaleViewSet, SaleItemViewSet, PlatformInvoiceViewSet,
-    TenantCustomerViewSet, TenantInvoiceViewSet, TenantPaymentViewSet,
-    TenantViewSet, TenantUserViewSet, SaleViewSet, UserViewSet
-)
-
 __all__ = [
     "BaseTenantViewSet",
     "ItemViewSet", "SaleViewSet", "SaleItemViewSet", 
