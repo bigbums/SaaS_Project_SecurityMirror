bb11b8738621be46b5ce0d92617196923b2ed53f
bigbums <bigbums@gmail.com>
2026-03-04 20:49:04 +0100

    Align viewsets exports: fix ItemViewSet import error

--- saas_app/core/viewsets/__init__.py
+++ saas_app/core/viewsets/__init__.py
@@ -1,1 +1,25 @@
-__all__ = []
+from .viewsets import (
+    UserViewSet,
+    ItemViewSet,
+    SaleViewSet,
+    SaleItemViewSet,
+    TenantCustomerViewSet,
+    TenantInvoiceViewSet,
+    TenantPaymentViewSet,
+    TenantViewSet,
+    TenantUserViewSet,
+    PlatformInvoiceViewSet,
+)
+
+__all__ = [
+    "UserViewSet",
+    "ItemViewSet",
+    "SaleViewSet",
+    "SaleItemViewSet",
+    "TenantCustomerViewSet",
+    "TenantInvoiceViewSet",
+    "TenantPaymentViewSet",
+    "TenantViewSet",
+    "TenantUserViewSet",
+    "PlatformInvoiceViewSet",
+]
