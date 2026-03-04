1c055d09883fb00a1bce3dcc887f1b6bc5d7222a
bigbums <bigbums@gmail.com>
2026-03-04 19:52:29 +0100

    Fix viewsets __init__.py: explicitly import all viewset classes

--- saas_app/core/viewsets/__init__.py
+++ saas_app/core/viewsets/__init__.py
@@ -1,6 +1,25 @@
+from .viewsets import (
+    BaseTenantViewSet,
+    ItemViewSet,
+    SaleViewSet,
+    SaleItemViewSet,
+    TenantCustomerViewSet,
+    TenantInvoiceViewSet,
+    TenantPaymentViewSet,
+    TenantViewSet,
+    TenantUserViewSet,
+    PlatformInvoiceViewSet,
+)
+
 __all__ = [
     "BaseTenantViewSet",
-    "ItemViewSet", "SaleViewSet", "SaleItemViewSet", 
-    "TenantCustomerViewSet", "TenantInvoiceViewSet", "TenantPaymentViewSet",
-    "TenantViewSet", "TenantUserViewSet",
+    "ItemViewSet",
+    "SaleViewSet",
+    "SaleItemViewSet",
+    "TenantCustomerViewSet",
+    "TenantInvoiceViewSet",
+    "TenantPaymentViewSet",
+    "TenantViewSet",
+    "TenantUserViewSet",
+    "PlatformInvoiceViewSet",
 ]
